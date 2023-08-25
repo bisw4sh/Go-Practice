67eb658834f85d7ef4ff13fe0d60982ad4afd0df
Biswash <59287553+bisw4sh@users.noreply.github.com>
2023-08-26 00:01:29 +0545

    queue using linked list

---
+++ 81 DSA/02 Queue/linkedList/queue_linkedList.go
@@ -0,0 +1,18 @@
+package main
+import "container/list"
+import "fmt"
+
+func main() {
+	// new linked list
+	queue := list.New()
+
+	// Simply append to enqueue.
+	queue.PushBack(10)
+	queue.PushBack(20)
+	queue.PushBack(30)
+
+	// Dequeue
+	front:=queue.Front()
+	fmt.Println(front.Value)
+	queue.Remove(front)
+}
