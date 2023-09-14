75602d4b9c9edb645f5d376aa69609dc29dad49c
Biswash <59287553+bisw4sh@users.noreply.github.com>
2023-09-14 07:23:11 +0545

    interface

---
+++ 02 DSA/19 Implementing Interfac e/implementing_interface.go
@@ -0,0 +1,28 @@
+package main
+import "fmt"
+
+type catalog interface { 
+   shipping() float64 
+   tax() float64
+} 
+ 
+type configurable struct { 
+   name string
+   price, qty float64   
+} 
+
+func (c *configurable) tax() float64{
+  return c.price * c.qty * 0.05
+}
+
+func (c *configurable) shipping() float64{
+  return c.qty * 5
+}
+
+func  main() {
+  tshirt := configurable{}
+  tshirt.price = 250
+  tshirt.qty = 2
+  fmt.Println("Shipping Charge: ", tshirt.shipping())
+  fmt.Println("Tax: ", tshirt.tax())  
+}
