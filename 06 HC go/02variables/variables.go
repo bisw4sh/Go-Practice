b04f1e2fd5db7ee0c282848b6acb9b2ee63cf65e
Biswash Dhungana <spikeystona@gmail.com>
2024-01-16 20:57:52 +0545

    hc

---
+++ 06 HC go/02variables/variables.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+
+func main() {
+	var user string = "Biswash"
+	surname := "Dhungana"
+	binaryVal := false
+	manyVal := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(user, surname)
+	fmt.Println(binaryVal, manyVal)
+}
